3b6f41f5acf050deee31eaaa134fce74c7db3f5f
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-09-12 16:04:40 -0400

    have projects displayed on a slideshow with a 3sec interval

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -17,3 +17,21 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 // Scroll to Top Button Ends
+
+// Slideshow my projects Starts
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+  const x = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
+// Slideshow my projects Ends
